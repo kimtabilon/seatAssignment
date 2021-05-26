8f2d7fadc4d371f8417e1bef9a3f7a0e3b78c7e8
Kim Tabilon <kim.gtabilon@gmail.com>
2021-05-26 22:04:20 +0800

    new: minesweepers

---
+++ Minesweeper.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+import java.util.HashMap; // import the HashMap class
+import java.util.HashSet; // Import the HashSet class
+
+// Import the ArrayList class and the Iterator class
+import java.util.Iterator;
+
+
+class Minesweeper
+{
+
+ public static void main(String ar[]) {
+ 	int matrix = 5;
+
+ 	HashSet<String> bomb = new HashSet<String>();
+ 	HashSet<String> active = new HashSet<String>();
+
+ 	bomb.add("1,2");
+
+ 	for(int i=0;i<30;i++) {
+	 	System.out.println("    1   2   3   4   5 ");
+	 	System.out.println("   ------------------- ");
+	 	for (int row=1; row<=matrix; row++) {
+	 		System.out.print(row+" |");
+	 		for (int column=1; column<=matrix; column++) {
+	 			if(active.contains(row+","+column)) {
+	 				System.out.print(" 1 |");
+	 			} else {
+	 				System.out.print("   |");
+	 			}
+	 		}
+	 		System.out.println("");
+	 		System.out.println("   ------------------- ");
+	 	}
+
+	 	Scanner sc=new Scanner(System.in);
+
+	 	System.out.println("Enter Row:");
+		int irow=sc.nextInt();
+
+		System.out.println("Enter Column:");
+		int icolumn=sc.nextInt();
+
+		Scanner sc2=new Scanner(System.in);
+
+		if( irow<=matrix 
+		&& irow>=1  
+		&& icolumn<=matrix  
+		&& icolumn>=1 
+		) {
+			if(active.contains(irow+","+icolumn)) {
+				System.out.println("Already selected. Enter to continue");
+				String already=sc2.nextLine();
+			} else {
+				if(bomb.contains(irow+","+icolumn)) {
+					System.out.println("Opps, bomb exploded. GAMEOVER");
+					break;
+				} else {
+					active.add(irow+","+icolumn);
+					// checking bombs for hints
+					boolean sorround = false;
+					int count = 0;	
+					if(bomb.contains((irow+1)+","+(icolumn+1))) {sorround = true; count++;}
+					if(bomb.contains((irow-1)+","+(icolumn-1))) {sorround = true; count++;}
+					if(bomb.contains(irow+","+(icolumn+1))) {sorround = true; count++;}
+					if(bomb.contains((irow+1)+","+icolumn)) {sorround = true; count++;}
+					if(bomb.contains(irow+","+(icolumn-1))) {sorround = true; count++;}
+					if(bomb.contains((irow-1)+","+icolumn)) {sorround = true; count++;}
+					
+					if(sorround) {
+						System.out.println("HINT : Soround "+count+" bomb/s. Enter to continue");
+						String hint=sc2.nextLine();
+					}
+						
+				}
+				
+			}
+
+		} else {
+			System.out.println("Invalid input for row/column. Enter to continue");
+			String invalid=sc2.nextLine();
+		}
+
+	 	System.out.print("\033[H\033[2J");
+		System.out.flush();
+ 	}
+
+ }
+
+}
